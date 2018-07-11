020751b1f4e03617a430495f779641c0f6adee32
Connor Gallopo <connor.gallopo@me.com>
2018-07-11 11:59:15 -0400

    added phrg rubocop and spec

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
 end
