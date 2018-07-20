f516724f3f7e27b643d5ce6bf5e82f516c163db6
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-20 10:28:39 -0400

    final corrections

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,8 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title,ingredients_attributes: [:name, :quantity])
+    params.require(:recipe).permit(
+      :title, ingredients_attributes: %i[name quantity]
+    )
   end
 end
