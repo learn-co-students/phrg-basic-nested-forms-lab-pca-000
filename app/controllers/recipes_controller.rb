93895b87ed4d5dfb6c9eaa0c9a69b9d8c20f79ba
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-20 10:27:14 -0400

    pre rubocop

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,7 +11,16 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
-  def create; end
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title,ingredients_attributes: [:name, :quantity])
+  end
 end
