2accfdb7fa27ac508fc8ce018bda97fc2b775f87
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-05-23 14:54:35 -0400

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe.ingredients.build if @recipe.ingredients.last.try(:name)
   end
 
   def index
@@ -11,7 +12,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    2.times { @recipe.ingredients.build }
   end
 
-  def create; end
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title, ingredients_attributes: %i[id name quantity]
+    )
+  end
 end
